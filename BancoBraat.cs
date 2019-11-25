300266b4ce21d239e35b50807a9485ca66401532
BrunoBraat <bruno.braat@gmail.com>
2019-11-25 17:11:16 -0300

    first commit

---
+++ BancoBraat.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicios.ProjetoBanco {
+    class BancoBraat {
+    
+        static void Main(string[] args) {
+
+            Console.Write("Entre com o número da conta: ");
+            int NumeroConta = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o titular da conta: ");
+            string TitularConta = Console.ReadLine();
+            char recebeSimOuNao = 'a';
+
+            while (recebeSimOuNao != 's' && recebeSimOuNao !=  'n') {
+                Console.Write("Haverá depósito inicial (s/n)? ");
+                recebeSimOuNao = char.Parse(Console.ReadLine());
+            }
+
+            Conta c = BancoBraat.CriarConta(recebeSimOuNao, NumeroConta, TitularConta);
+
+            Console.Write($"Entre um valor para depósito: ");
+            c.Depositar(decimal.Parse(Console.ReadLine()));
+
+            Console.Write($"Entre um valor para sacar: ");
+            c.Sacar(decimal.Parse(Console.ReadLine()));
+            
+        }
+
+
+
+        public static Conta CriarConta(char recebeSimOuNao, int NumeroConta, string TitularConta) {
+
+            char sim = 's';
+
+            Conta c = null;
+
+         
+            if (recebeSimOuNao == sim) {
+                Console.WriteLine("Entre o valor de depósito inicial: ");
+                decimal recebeSaldo = decimal.Parse(Console.ReadLine());
+                c = new Conta(TitularConta, NumeroConta, recebeSaldo);
+
+            } else {
+
+                c = new Conta(TitularConta, NumeroConta);       
+
+                }
+
+            c.InformarConta();
+
+            return c;
+
+        }
+
+
+    }
+}
---
+++ Conta.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicios.ProjetoBanco {
+    class Conta {
+        public int NumeroConta { get; }
+        public string TitularConta { get; set; }
+
+        //Propriedade Saldo
+        public static decimal Saldo { get; set; }
+
+        public void Depositar(decimal valor) {
+
+            Saldo += valor;
+            this.InformarConta();
+        }
+
+        //Construtor com saldo incial
+        public Conta(string titularConta, int numeroConta, decimal saldoInicial) {
+
+            TitularConta = titularConta;
+            NumeroConta = numeroConta;
+            Saldo = saldoInicial;
+        }
+
+        // Construtor com cliente e conta
+
+        public Conta(string titularConta, int numeroConta) {
+
+            TitularConta = titularConta;
+            NumeroConta = numeroConta;
+
+        }
+
+        public void Sacar(decimal valor) {
+
+            Saldo -= valor;
+            this.InformarConta();
+
+
+        }
+
+        public void InformarConta() {
+
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine($"Conta {NumeroConta},Titular: {TitularConta}, Saldo: {Saldo}");
+
+        }
+
+    }
+}
